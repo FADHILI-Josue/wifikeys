015142fdfaa08ca40c2daa15daa9e8a6e844a75a
FADHILI Josue <fadhilijosue27@gmail.com>
2024-05-11 12:44:35 +0200

    fix:shebang line issue

--- src/bin.ts
+++ src/bin.ts
@@ -1,48 +1,5 @@
-// #!/usr/bin/env node
+#!/usr/bin/env node
 
-import {Command} from 'commander';
-import consola from 'consola';
-import pkg from '../package.json';
-import getWifiPassword from './utils/wifi-password';
-
-const program = new Command();
-
-program
-  .name('wifikeys')
-  .description('get wifi keys(passwords) CLI')
-  .usage('command [arguments]')
-  .version(
-    `\u001B[1mv${pkg.version}\u001B[0m`,
-    '-v, --version',
-    "Output wifikeys's current version.",
-  )
-  .helpOption('-h, --help', 'Output usage of wifikeys.');
-
-program
-  .command('get [ssid]')
-  .description('get password for wifi')
-  .option('-n, --networks  <networks...>', 'wifi networks to get passwords for')
-  .action(async (ssid: string, options) => {
-    try {
-      if (options?.networks?.length) {
-        const promises = (options.networks as string[]).map(async (ssid) => {
-          const password: string = await getWifiPassword(ssid);
-          console.log(`> ${ssid}: ${password} \n`);
-        });
-
-        // Wait for all promises to resolve
-        await Promise.all(promises);
-        process.exit(0);
-      }
-
-      await getWifiPassword(ssid).then(console.log);
-      process.exit(0);
-    } catch {
-      consola.error(
-        'failed to get password!! check if the you have saved password for wifi network',
-      );
-      process.exit(0);
-    }
-  });
+import { program } from "./command";
 
 program.parse(process.argv);
